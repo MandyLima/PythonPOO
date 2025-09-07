3c995659ecd22ea1ec142ac66f55b31377cd1c7f
MandyLima <161380181+MandyLima@users.noreply.github.com>
2025-09-07 20:34:20 -0300

    Criando subclasse

---
+++ Comissionado.py
@@ -0,0 +1,22 @@
+from Funcionario import Funcionario 
+
+class Comissionado(Funcionario):
+
+    vendas:int
+
+    def __init__(self, cod, nome, valorHora, horasTrabalhadas, vendas):
+        super().__init__(cod, nome, valorHora, horasTrabalhadas) 
+        self.vendas = vendas      
+        self.salario = self.calcularSalario()
+        
+    def calcularSalario(self):
+        salario = self.valorHora*self.horasTrabalhadas
+        return salario*1.15
+    
+    def exibirComissionado(self):
+        print(f"==== Funcionario Comissionado ====")
+        print(f"Código: {self.cod} \nNome: {self.nome} \nValor de horas: {self.valorHora} \nHoras Trabalhadas: {self.horasTrabalhadas} \nVendas: {self.vendas} \nSalário: {self.salario:.1f}")
+
+fc2 = Comissionado(2, "Juliana", 25, 30, 5) 
+fc2.exibirComissionado()
+        
